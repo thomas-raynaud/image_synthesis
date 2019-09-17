bb8b839dd20e2cfccd76c0865d1558e5d442cb4f
thomas <thomas.raynaud@etu.univ-lyon1.fr>
2019-09-17 09:36:34 +0200

    material works

--- tps/tp1_quaternius/tp1_quaternius.cpp
+++ tps/tp1_quaternius/tp1_quaternius.cpp
@@ -11,40 +11,91 @@
 #include "program.h"
 #include "uniforms.h"
 
+#define NB_MESHES 24
+
 struct Buffers
 {
     GLuint vao;
     GLuint vertex_buffer1;
     GLuint vertex_buffer2;
+    GLuint normal_buffer1;
+    GLuint normal_buffer2;
+    GLuint material_buffer;
+    GLuint material_index_buffer;
+    Mesh meshes[NB_MESHES];
     int vertex_count;
 
-    Buffers( ) : vao(0), vertex_buffer1(0), vertex_buffer2(0), vertex_count(0) {}
-    
-    void create(const Mesh& mesh1,  const Mesh& mesh2)
-    {
-        if(!mesh1.vertex_buffer_size() || !mesh1.vertex_buffer_size()) return;
-        
-        // Mesh 1
-        glGenBuffers(1, &vertex_buffer1);
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer1);
-        glBufferData(GL_ARRAY_BUFFER, mesh1.vertex_buffer_size(), mesh1.vertex_buffer(), GL_STATIC_DRAW);
+    Buffers( ) : vao(0), vertex_buffer1(0), vertex_buffer2(0),
+                 normal_buffer1(0), normal_buffer2(0),
+                 material_buffer(0), material_index_buffer(0),
+                 vertex_count(0) {}
 
+    void create() {
+        glGenBuffers(1, &vertex_buffer1);
+        glGenBuffers(1, &vertex_buffer2);
+        glGenBuffers(1, &normal_buffer1);
+        glGenBuffers(1, &normal_buffer2);
+        glGenBuffers(1, &material_buffer);
+        glGenBuffers(1, &material_index_buffer);
         glGenVertexArrays(1, &vao);
+
+        // Materiaux
+        // creer et remplir le buffer avec les parametres des matieres.
+        glBindBuffer(GL_UNIFORM_BUFFER, material_buffer);
+        vec3 material_colors[meshes[0].mesh_materials().size()];
+        Color color_temp;
+        for (unsigned int i = 0; i < meshes[0].mesh_materials().size(); ++i) {
+            color_temp = meshes[0].mesh_materials()[i].diffuse;
+            material_colors[i] = vec3(color_temp.r, color_temp.g, color_temp.b);
+        }
+        glBufferData(GL_UNIFORM_BUFFER, meshes[0].mesh_materials().size() * sizeof(vec3), material_colors, GL_STATIC_DRAW);
+       
+        // creer et remplir le buffer contenant l'indice de la matiere de chaque triangle
+        glBindBuffer(GL_UNIFORM_BUFFER, material_index_buffer);
+        GLuint material_triangle_index[meshes[0].materials().size()];
+        for (unsigned int i = 0; i < meshes[0].materials().size(); ++i) {
+            material_triangle_index[i] = meshes[0].materials()[i];
+        }
+        glBufferData(GL_UNIFORM_BUFFER, meshes[0].materials().size(), material_triangle_index, GL_STATIC_DRAW);
+    }
+    
+    void loadMeshes(const int ind_mesh1, const int ind_mesh2)
+    {
+        if(!meshes[ind_mesh1].vertex_buffer_size() || !meshes[ind_mesh2].vertex_buffer_size()) return;
+
+        // configure le vertex array object: conserve la description des attributs de sommets
         glBindVertexArray(vao);
         
+        // initialise le buffer du mesh 1: conserve la positions des sommets
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer1);
+        glBufferData(GL_ARRAY_BUFFER, meshes[ind_mesh1].vertex_buffer_size(), meshes[ind_mesh1].vertex_buffer(), GL_STATIC_DRAW);
+        // attribut 0, position des sommets du mesh 1, declare dans le vertex shader : in vec3 position1;
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArray(0);
 
-        // Mesh 2
-        glGenBuffers(1, &vertex_buffer2);
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer2);
-        glBufferData(GL_ARRAY_BUFFER, mesh2.vertex_buffer_size(), mesh2.vertex_buffer(), GL_STATIC_DRAW);
-
+        // initialise le buffer du mesh 1: normales
+        glBindBuffer(GL_ARRAY_BUFFER, normal_buffer1);
+        glBufferData(GL_ARRAY_BUFFER, meshes[ind_mesh1].normal_buffer_size(), meshes[ind_mesh1].normal_buffer(), GL_STATIC_DRAW);
+        // attribut 1, position des sommets du mesh 1, declare dans le vertex shader : in vec3 position1;
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArray(1);
+
+        // initialise le buffer du mesh 2: conserve la positions des sommets
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer2);
+        glBufferData(GL_ARRAY_BUFFER, meshes[ind_mesh2].vertex_buffer_size(), meshes[ind_mesh2].vertex_buffer(), GL_STATIC_DRAW);
+        // attribut 2, position des sommets du mesh 2, declare dans le vertex shader : in vec3 position2;
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(2);
+
+        // initialise le buffer du mesh 2: normales
+        glBindBuffer(GL_ARRAY_BUFFER, normal_buffer2);
+        glBufferData(GL_ARRAY_BUFFER, meshes[ind_mesh2].normal_buffer_size(), meshes[ind_mesh2].normal_buffer(), GL_STATIC_DRAW);
+        // attribut 1, position des sommets du mesh 1, declare dans le vertex shader : in vec3 position1;
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(3);
         
         // conserve le nombre de sommets
-        vertex_count= mesh1.vertex_count();
+        vertex_count = meshes[ind_mesh1].vertex_count();
 
         // nettoyage
         glBindVertexArray(0);
@@ -53,8 +104,15 @@ struct Buffers
     
     void release( )
     {
+        for (int i = 0; i < NB_MESHES; ++i) {
+            meshes[i].release();
+        }
         glDeleteBuffers(1, &vertex_buffer1);
         glDeleteBuffers(1, &vertex_buffer2);
+        glDeleteBuffers(1, &normal_buffer1);
+        glDeleteBuffers(1, &normal_buffer2);
+        glDeleteBuffers(1, &material_buffer);
+        glDeleteBuffers(1, &material_index_buffer);
         glDeleteVertexArrays(1, &vao);
     }
 };
@@ -69,11 +127,22 @@ public:
     // creation des objets de l'application
     int init( )
     {
-        // creer le vertex buffer et le vao
-        m_objet.create(read_mesh("tobot_quaternius/cube.obj"), m_texture);
-        m_camera.lookat(Point(-1, -1, -1), Point(10, 10, 1));
+        m_objet.meshes[0] = read_mesh("robot_quaternius/Robot.obj");
+        // charger les meshes
+        std::string mesh_path;
+        for (int i = 1; i < NB_MESHES; ++i) {
+            mesh_path.clear();
+            mesh_path = "robot_quaternius/run/Robot_0000";
+            if (i < 10) mesh_path += "0";
+            mesh_path += std::to_string(i) + ".obj";
+            m_objet.meshes[i] = read_mesh(mesh_path.c_str());
 
-        m_program= read_program("tps/tp1/tp1_quaternius.glsl");
+        }
+        // charger les vertex buffers et le vao
+        m_objet.create();
+        m_camera.lookat(Point(0, 2, 5), Point(0, 2, 0));
+
+        m_program = read_program("tps/tp1_quaternius/tp1_quaternius.glsl");
         program_print_errors(m_program);
 
         
@@ -93,16 +162,15 @@ public:
     {
         release_program(m_program);
         m_objet.release();
-        glDeleteTextures(1, &m_texture);
         
         return 0;
     }
     
     // dessiner une nouvelle image
-    int render( )
+    int render()
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        float gt = global_time();
         // deplace la camera
         int mx, my;
         unsigned int mb= SDL_GetRelativeMouseState(&mx, &my);
@@ -113,36 +181,43 @@ public:
         else if(mb & SDL_BUTTON(2))         // le bouton du milieu est enfonce
             m_camera.translation((float) mx / (float) window_width(), (float) my / (float) window_height());
 
-        glBindVertexArray(m_objet.vao);
-
         glUseProgram(m_program);
-        program_uniform(m_program, "texture", m_texture);
         Transform projection= m_camera.projection(window_width(), window_height(), 45);
-        Transform mvp, vp = projection * m_camera.view();
+        Transform mvp = projection * m_camera.view() * Identity();
+        program_uniform(m_program, "mMatrix", Identity());
+        program_uniform(m_program, "mvpMatrix", mvp);
+        program_uniform(m_program, "normalMatrix", Identity().normal());
+        program_uniform(m_program, "light", vec3(10, 10, 5));
+        float speed = 12000.f;
+        int t0 = (gt / (speed / 24.f));
+        int t1 = t0 + 1;
+        int kf1 = t0 % 23 + 1;
+        int kf2 = t1 % 23 + 1;
+        float t = (gt / (speed / 24.f));
+        float dt = (t - t0) / (t1 - t0);
+        m_objet.loadMeshes(kf1, kf2);
+        float time = ((int)t % 1000) / speed;
+        program_uniform(m_program, "time", dt);
+
+        // recuperer l'identifiant du block a associer au buffer
+        GLuint material_block = glGetUniformBlockIndex(m_program, "MaterialBlock");
+        // les uniform blocks sont numerotes, et c'est l'application qui doit choisir le numero...
+        // associe le numero 0 au bloc "MaterialBLock"
+        glUniformBlockBinding(m_program, material_block, 0);      
+        // associe le contenu d'un buffer au block numero 0
+        glBindBufferBase(GL_UNIFORM_BUFFER, 0, m_objet.material_buffer);
+
+        // on recommence pour les indices, avec un autre numero
+        GLuint index_block = glGetUniformBlockIndex(m_program, "IndexBlock");
+        glUniformBlockBinding(m_program, index_block, 1);
+        glBindBufferBase(GL_UNIFORM_BUFFER, 1, m_objet.material_index_buffer);
+
+        // selectionner les attributs et les buffers de l'objet
+        glBindVertexArray(m_objet.vao);
         
-        
-        //draw(m_objet, m_camera, m_texture);
-        Transform model;
-        double nbBoxes = 100;
-        for (int i = 0; i < sqrt(nbBoxes); ++i) {
-            for (int j = 0; j < sqrt(nbBoxes); ++j) {
-                model = Identity();
-                mvp = vp * Identity();
-                program_uniform(m_program, "mvpMatrix", mvp);
-                program_use_texture(m_program, "color_texture", 0, m_texture, 0);
-                //draw(m_objet, m_program);
-                //draw(m_objet, model, m_camera, m_texture);
-                // selectionner les attributs et les buffers de l'objet
-                glBindVertexArray(m_objet.vao);
-                
-                // dessiner les triangles de l'objet
-                glDrawArrays(GL_TRIANGLES, 0, m_objet.vertex_count);
-                // nettoyage
-                glUseProgram(0);
-                glBindVertexArray(0);
-                return 1;
-            }
-        }
+        // dessiner les triangles de l'objet
+        glDrawArrays(GL_TRIANGLES, 0, m_objet.vertex_count);
+
         // nettoyage
         glUseProgram(0);
         glBindVertexArray(0);
@@ -151,7 +226,6 @@ public:
 
 protected:
     Buffers m_objet;
-    GLuint m_texture;
     GLuint m_program;
     Orbiter m_camera;
     
--- tps/tp1_quaternius/tp1_quaternius.glsl
+++ tps/tp1_quaternius/tp1_quaternius.glsl
@@ -1,31 +1,61 @@
 #version 330
 
 #ifdef VERTEX_SHADER
-layout(location= 0) in vec3 position;
-layout(location= 1) in vec2 texcoord;
-out vec2 vertex_texcoord;
+layout(location=0) in vec3 position1;
+layout(location=1) in vec3 normal1;
+layout(location=2) in vec3 position2;
+layout(location=3) in vec3 normal2;
 
+uniform mat4 mMatrix;
 uniform mat4 mvpMatrix;
+uniform mat4 normalMatrix;
+uniform float time;
 
-void main( )
+out vec3 vertex_normal;
+out vec3 vertex_position;
+
+void main()
 {
-    gl_Position= mvpMatrix * vec4(position, 1);
-    vertex_texcoord = texcoord;
+    vec3 p;
+    p = position1 * (1.0 - time) + position2 * time;
+    gl_Position= mvpMatrix * vec4(p, 1);
+    vertex_normal = mat3(normalMatrix) * ((normal1 * (1.0 - time)) + (normal2 * time));
+    vertex_position= vec3(mMatrix * vec4(position1, 1));
 }
 
 #endif
 
 
 #ifdef FRAGMENT_SHADER
-in vec2 vertex_texcoord;
+
+in vec3 vertex_normal;
+in vec3 vertex_position;
 out vec4 fragment_color;
-uniform sampler2D color_texture;
 
+uniform vec3 light;
 
-void main( )
+uniform MaterialBlock
 {
-    vec4 color= texture(color_texture, vertex_texcoord);
-    fragment_color= color;
+    vec3 colors[256];
+};
+uniform IndexBlock
+{
+    int color_indices[256];
+};
+
+
+void main()
+{
+    int color_index = color_indices[gl_PrimitiveID];
+    vec3 color = colors[color_index];
+    //vec3 color = vec3(1, 1, 1);
+    vec3 normal = normalize(vertex_normal);
+
+    float cos_theta = 1;
+    cos_theta = max(0, dot(normal, normalize(light - vertex_position))); // eclairage, uniquement des faces bien orientees
+    
+    color.rgb = color.rgb * cos_theta;
+    fragment_color = vec4(color, 1);
 }
 
 #endif
